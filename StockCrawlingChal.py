1b889998d85ff3fd10461ced8aa135aa1f42b9a0
Boschko <25066959+OlivierLaflamme@users.noreply.github.com>
2019-10-07 14:52:00 -0400

    Create  StockCrawlingChal.py

---
+++ StockCrawlingChal.py
@@ -0,0 +1,62 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import traceback
+
+def getHTMLText(url):
+    try:
+        r = requests.get(url,timeout = 30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return ""
+
+def getStockList(lst,stockURL):
+    html = getHTMLText(stockURL)
+    soup = BeautifulSoup(html,"html.parser")
+    a = soup.find_all("a")
+    for i in a:
+        try:
+            href = i.attrs["href"]
+            lst.append(re.find_all(r"[s][hz]\d{6}",href)[0])
+        except:
+            continue
+
+def getStockInfo(lst,stockURL,fpath):
+    for stock in lst:
+        url = stockURL + stock + ".html"
+        html = getHTMLText(url)
+        try:
+            if html =="":
+                continue
+            infoDict = {}
+            soup = BeautifulSoup(html,"html.parser")
+            stockInfo = soup.find("div",attrs={"class":"stick-bets"})
+
+            name = stockInfo.find_all(attrs={"class":"bets-name"})[0]
+            infoDict.update({"股票名称":name.text.split()[0]})
+
+            keyList = stockInfo.find_all("dt")
+            valueList = stockInfo.find_all("dd")
+            for i in range(len(keyList)):
+                key = keyList[i].text
+                val = valueList[i].text
+                infoDict[key] = val
+
+            with open(fpath,"a",encoding="utf-8") as f:
+                f.write(str(infoDict)+"\n")
+        except:
+            traceback.print_exc()
+            continue
+
+
+def main():
+    stock_list_url = "http://quote.eastmoney.com/stocklist.html"
+    stock_info_url = "https://gupiao.baidu.com/stock/"
+    output_file = "D://BaiduStockInfo.txt"
+    slist = []
+    getStockList(slist,stock_list_url)
+    getStockInfo(slist,stock_info_url,output_file)
+
+main()
