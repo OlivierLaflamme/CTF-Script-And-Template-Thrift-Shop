d9e94df8ce5084d598e6c34f7e5c46e2b9dd3cc5
Boschko <25066959+OlivierLaflamme@users.noreply.github.com>
2019-10-24 10:25:19 -0400

    Create load_the_config_file.py

---
+++ load_the_config_file.py
@@ -0,0 +1,8 @@
+import configparser
+cfg = configparser.ConfigParser()
+cfg.read('Config.ini')
+user = cfg.get("Server", "username")
+passwd = cfg.get("Server", "password")
+host = cfg.get("Server", "host")
+Dbname = cfg.get("Server","db")
+thread_s = cfg.get("Config","thread_s")
