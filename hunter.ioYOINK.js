58927e5f368ca08bb84ad2781c4b3af6ad1ea19b
a h%00r�ef="google.com"Name/a'=0Attt (Boschko) <25066959+OlivierLaflamme@users.noreply.github.com>
2020-02-24 11:56:23 -0500

    Create hunter.ioYOINK.js

---
+++ hunter.ioYOINK.js
@@ -0,0 +1,9 @@
+takeemail = document.getElementsByClassName('email-container')
+
+takeemail.length
+
+for (i = 0; i < takeemail.length; i++) {
+  console.log(takeemail[i].innerText);
+}
+
+echo " " | sed 's/\s.*//g' | sort -uV
